cdfaa5c67598c824d7f7e4be4a85518644612f53
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-09-24 15:39:14 +0200

    Add files via upload

---
+++ PravokutniTrokut/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PravokutniTrokut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+        start:
+
+            Console.WriteLine("Unesite duljinu stranice x: ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite duljinu stranice y: ");
+            y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite duljinu stranice z: ");
+            z = int.Parse(Console.ReadLine());
+
+            if (x > 0 && y > 0 && z > 0)
+            {
+
+                if (z == Math.Sqrt((x * x) + (y * y)))
+                {
+                    Console.WriteLine("Ove stranice pripadaju pravokutnom trokutu");
+                }
+
+                else
+                {
+                    Console.WriteLine("Ove stranice ne pripadaju pravokutnome trokutu");
+                }
+
+            }
+
+            else
+            {
+                goto start;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
